5cbf8701113d7975ed0ec332b217d59434855e2d
Ivan Borisenko <myivanbor@gmail.com>
2023-05-17 18:57:18 +0400

    Начало работы

---
+++ Lesson1/main.py
@@ -0,0 +1,7 @@
+print (round(1.4))  # = 1
+print (round(1.5))  # = 2
+print (round(2.4))  # = 2
+print (round(2.5))  # = 2
+print (round(2.6))  # = 3
+print (round(-2.4)) # = -2
+print (round(-3.6)) # = -4
