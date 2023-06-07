02d4b7eb4effa00ec7c8a588e496910b80db3d44
Ivan Borisenko <myivanbor@gmail.com>
2023-06-08 02:23:12 +0400

    Решение домашних задач : 30_32

--- Homework/task28.py
+++ Homework/task28.py
@@ -5,28 +5,29 @@
 Также нельзя использовать циклы.
 Input: 2> 2>   Output: 4
 '''
-## Проверка введенного числа:
-try:
-    a = int(input('  Введите натуральное число А: '))
-except ValueError:
-    print("  Вы ввели не число!")
-    a = int(input('  Введите число А: '))
-try:
-    b = int(input('  Введите натуральное число А: '))
-except ValueError:
-    print("  Вы ввели не число!")
-    b = int(input('  Введите число В: '))
+### ВАРИАНТ 1 (мое решение):
+# ## Проверка введенного числа:
+# try:
+#     a = int(input('  Введите натуральное число А: '))
+# except ValueError:
+#     print("  Вы ввели не число!")
+#     a = int(input('  Введите число А: '))
+# try:
+#     b = int(input('  Введите натуральное число А: '))
+# except ValueError:
+#     print("  Вы ввели не число!")
+#     b = int(input('  Введите число В: '))
 
 
-def recursive_sum(a, b):
-    if a == 0: # базовое условие рекурсии
-        return b
-    else:
-        return recursive_sum (a - 1, b + 1) # рекурсивный вызов
+# def recursive_sum(a, b):
+#     if a == 0: # базовое условие рекурсии
+#         return b
+#     else:
+#         return recursive_sum (a - 1, b + 1) # рекурсивный вызов
 
-print (f'Сумма А + В = {recursive_sum(a, b)}')
+# print (f'Сумма А + В = {recursive_sum(a, b)}')
 
-
+### ВАРИАНТ 2 ():
 # def summa(a, b):
 #     a += 1
 #     b -= 1
@@ -34,5 +35,16 @@ print (f'Сумма А + В = {recursive_sum(a, b)}')
 #         return summa(a, b)
 #     else:
 #         return a
- 
 # print (f'Сумма А + В = {summa(a, b)}')
+
+
+### ВАРИАНТ 3 (от преподавателя)
+### Сергей Сердюк:
+def sum(c, d):
+  if d == 0:
+    return c
+  return (c, d - 1) + 1
+
+n = int(input())
+m = int(input())
+print(sum(n, m))
--- Homework/task37.py
+++ Homework/task37.py
@@ -17,16 +17,29 @@ Input: 2 -> 3 4 Output: 4 3
 5-выводим результат на экран
 
 '''
-print('Задача 37')
-print('Провести инверсию натурального числа N:')
-N = str(input('  Введите натуральное число N: '))
+### ВАРИАНТ 1 (мое решение):
+# print('Задача 37')
+# print('Провести инверсию натурального числа N:')
+# N = str(input('  Введите натуральное число N: '))
 
-# Метод инверсии строки 
-def reverse(string):
-    if len(string) == 0:
-        return string
-    else:
-        return reverse(string[1:]) + string[0]
+# # Метод инверсии строки 
+# def reverse(string):
+#     if len(string) == 0:
+#         return string
+#     else:
+#         return reverse(string[1:]) + string[0]
     
-print(N)
-print(reverse(N))
+# print(N)
+# print(reverse(N))
+
+
+### ВАРИАНТ 2 (от преподавателя)
+### Сергей Сердюк 22:22
+def f(n):
+    if n == 0:
+        return ''
+    k = int(input())
+    return f(n - 1) + f' {k}'
+
+n = int(input())
+print(f(n))
